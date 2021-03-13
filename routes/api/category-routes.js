14f54b44fab24300a1c986e090d087165deb74d4
dvaghani <dhruvi.vaghani@gmail.com>
2021-03-12 23:41:28 -0500

    saving changes to category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,11 +4,33 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  try {
+    const allCatergoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(allCatergoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all categories
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  try {
+    const allCatergoryData = await Category.findbyPk(req.params.id, {
+      include: [{ model: Product }],
+    })
+
+    if (!allCatergoryData) {
+      res.status(404).json({ message: 'No category found with that id!'});
+      return;
+    }
+
+    res.status(200).json(allCatergoryData);
+  } catch (err) {
+    res.status(500),json(err);
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
