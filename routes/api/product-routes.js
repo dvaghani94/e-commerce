c4ec265c018139fab1a6ea4c8a0f461b1728a83f
dvaghani <dhruvi.vaghani@gmail.com>
2021-03-13 21:47:03 -0500

    saving all files, only readme left

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -64,10 +64,11 @@ router.post('/', (req, res) => {
     product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
-    category_id: req.body.tagIds,
+    tagIds: req.body.tagIds,
     // tagIds: req.body.tagIds,
   })
     .then((product) => {
+      console.log(product)
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
